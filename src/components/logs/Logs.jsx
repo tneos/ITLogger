c3296fe460736aeb9c8e2da27ab4ca71137590bd
Thomas Neos <tneos@hotmail.com>
2024-05-04 12:25:33 +0100

    Add LogItem, Preloader components and format date

--- src/components/logs/Logs.jsx
+++ src/components/logs/Logs.jsx
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from "react";
+import LogItem from "./LogItem";
+import Preloader from "../layout/Preloader";
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  // Fetch array of logs and set component state when app loads
   const getLogs = async () => {
     setLoading(true);
     const res = await fetch("/logs");
@@ -21,18 +24,18 @@ const Logs = () => {
   console.log(loading);
 
   if (loading) {
-    return <h4>Loading..</h4>;
+    return <Preloader />;
   }
 
   return (
-    <ul className="collection-with-header">
+    <ul className="collection with-header">
       <li className="collection-header">
         <h4 className="center">System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
         <p className="center">No Logs to show..</p>
       ) : (
-        logs.map(log => <li>{log.message}</li>)
+        logs.map(log => <LogItem log={log} key={log.id} />)
       )}
     </ul>
   );
