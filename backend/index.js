765a7ad1dfe291b17748758127f0961804011cb2
Thomas Neos <tneos@hotmail.com>
2024-05-29 11:41:56 +0100

    Log data

--- backend/index.js
+++ backend/index.js
@@ -11,16 +11,16 @@ app.use(express.json({extended: false}));
 
 app.use(express.json());
 
+// Define Routes
+app.use("/logs", require("./src/routes/logs"));
+app.use(`/techs`, require("./src/routes/techs"));
+
 mongoose.connect(process.env.MONGODB_URI).then(() => console.log("DB connection successful.."));
 
 if (process.env.MODE !== "development") {
   app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "../client/dist/index.html")));
 }
 const PORT = process.env.PORT || 5000;
-
-// Define Routes
-app.use("/logs", require("./src/routes/logs"));
-app.use(`/techs`, require("./src/routes/techs"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
