8ab638de1599331cde97eac35011d521388fbaf5
Thomas Neos <tneos@hotmail.com>
2024-05-29 17:00:11 +0100

    Read vite .env variables

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,25 @@
-import {defineConfig} from "vite";
+import {defineConfig, loadEnv} from "vite";
 import dotenv from "dotenv";
 import react from "@vitejs/plugin-react";
 
 dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BACKEND,
-  server: {
-    proxy: {
-      "/logs": "http://localhost:5000",
-      "/techs": "http://localhost:5000",
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    server: {
+      proxy: {
+        "/logs": "http://localhost:5000",
+        "/techs": "http://localhost:5000",
+      },
     },
-  },
-  plugins: [react()],
-  resolve: {
-    mainFields: [],
-  },
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    resolve: {
+      mainFields: [],
+    },
+  };
 });
